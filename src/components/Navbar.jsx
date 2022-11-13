6b920e3b705e197e58263369059a0eadc03ed742
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-13 16:15:44 +0100

    create a navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Flex>
+      <Text>Bg</Text>
+      <Box>
+        <Button>Premium</Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
