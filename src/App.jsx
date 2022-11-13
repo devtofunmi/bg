9cc1e8c1c6915f4d1acf2ed30a8d54c6c6d2f9a9
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-13 16:02:43 +0100

    add the homepage in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Homepage from "./pages/Homepage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box w={"100%"} minH={"100vh"} bg={"#181819"} color={"white"}>
+        <Homepage />
+      </Box>
+    </>
+  );
 }
 
 export default App;
