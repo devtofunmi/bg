2b34a24d0ccc3f4fa83d7f7b246561ec29f2efda
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-14 09:47:39 +0100

    ..

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Img, Text } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
-
+import bg from "../assets/choose.png";
 const Homepage = () => {
   return (
     <>
-      <Box h={"100vh"}>
+      <Box h={"100vh"} mx={"50px"}>
         <Navbar />
+        <Flex justifyContent={"space-between"} mt={"20px"}>
+          <Img w={"500px"} src={bg} />
+          <Box w={"600px"}>
+            <Text fontSize={"40px"} fontWeight={"bold"} fontStyle={"Roboto"}>
+              Remove Background From Image and Add Another Background for Free
+            </Text>
+          </Box>
+        </Flex>
       </Box>
     </>
   );
