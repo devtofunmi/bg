0cccdbaad04f41e32db70eddcca1ef95696a85c4
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-11-15 23:47:49 +0100

    rearrage the text box and add input file

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,18 @@
-import { Box, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, Input, Text } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
+import Dropzone from "../components/Dropzone";
 import bg from "../assets/choose.png";
 const Homepage = () => {
   return (
     <>
       <Box h={"100vh"} mx={"70px"}>
         <Navbar />
-        <Flex justifyContent={"space-between"} mt={"10px"}>
+        <Flex justifyContent={"space-between"} mt={"30px"}>
           <Box>
+            <Img w={"350px"} src={bg} />
+          </Box>
+          <Box w={"600px"}>
             <Box w={"500px"}>
               <Text fontSize={"50px"} fontWeight={"bold"} fontStyle={"Roboto"}>
                 Make Image Look Better
@@ -17,12 +21,9 @@ const Homepage = () => {
                 100% Automatically and Free
               </Text>
             </Box>
-            <Img w={"350px"} src={bg} />
-          </Box>
-          <Box w={"600px"}>
-            <Text fontSize={"50px"} fontWeight={"bold"} fontStyle={"Roboto"}>
-              Turn Any Photo Into Awersome and Make it Look Better
-            </Text>
+            <Box mt={"30px"}>
+              <Input type="file" />
+            </Box>
           </Box>
         </Flex>
       </Box>
